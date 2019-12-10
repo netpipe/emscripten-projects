40d8f0dd6b9b1882b1e689e1c2f25e043916b617
teslaenergy@gmail.com <teslaenergy@gmail.com>
2019-12-10 10:50:00 -0700

    build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+##!/bin/bash
+WIP extras
+
+cd assimp-3.3.1
+emcmake cmake . -DBUILD_SHARED_LIBS=OFF
+cp ./lib/libassimp.a ../bin/
+#libzlibstatic.a
+cd ..
+
+
+
+put new glfw into opensteer and then copied osmesa.h to GL folder inside source tree
+compiled with emcmake cmake -DGLFW_USE_OSMESA=1
