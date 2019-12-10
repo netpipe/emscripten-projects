a70363f0cfe663482ae01c5bd10875417f128fc8
teslaenergy@gmail.com <teslaenergy@gmail.com>
2019-12-10 11:16:07 -0700

    opensteer

--- AI/OpenSteer/CMakeFiles/OpenSteerDemo.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "OpenSteerDemo.pdb"
-  "OpenSteerDemo.js"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/OpenSteerDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
