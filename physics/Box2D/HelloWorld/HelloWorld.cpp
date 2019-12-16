9ccaf26ae91b15124db3d6525cd4507f82660d2c
teslaenergy@gmail.com <teslaenergy@gmail.com>
2019-12-15 20:07:32 -0700

    freetype2

--- physics/Box2D/HelloWorld/HelloWorld.cpp
+++ physics/Box2D/HelloWorld/HelloWorld.cpp
@@ -34,7 +34,7 @@ int main(int argc, char** argv)
 	b2Vec2 gravity(0.0f, -10.0f);
 
 	// Construct a world object, which will hold and simulate the rigid bodies.
-	b2World world(gravity);
+	b2World world(gravity,true);
 
 	// Define the ground body.
 	b2BodyDef groundBodyDef;
