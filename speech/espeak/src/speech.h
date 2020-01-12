358739a13478ce98c70c303def6d0188cd08d595
netpipe <teslaenergy@gmail.com>
2020-01-12 02:25:38 -0700

    cbp files

--- speech/espeak/src/speech.h
+++ speech/espeak/src/speech.h
@@ -62,7 +62,7 @@
 
 // will look for espeak_data directory here, and also in user's home directory
 #ifndef PATH_ESPEAK_DATA
-   #define PATH_ESPEAK_DATA  "/usr/share/espeak-data"
+   #define PATH_ESPEAK_DATA  "./usr/share/espeak-data"
 #endif
 
 typedef unsigned short USHORT;
